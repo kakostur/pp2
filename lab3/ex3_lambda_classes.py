73bbad89b23f289853566c132ea14f15fdc5e155
kakostur <tursynbaevakarakat@gmail.com>
2024-02-05 17:26:01 +0600

    add exercises lambda, classes

---
+++ lab3/ex3_lambda_classes.py
@@ -0,0 +1,23 @@
+#lambda
+x = lambda a:a
+
+#classes
+class MyClass:
+  x = 5
+
+
+class MyClass:
+  x = 5
+p1=MyClass()
+
+
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+
+
+class Person:
+  def  init (self, name, age):
+    self.name = name
+    self.age = age
