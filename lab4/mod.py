4daa8344834c6ef382f4d5d2ede496d1d25302e4
kakostur <tursynbaevakarakat@gmail.com>
2024-02-13 22:47:55 +0600

    molule

---
+++ lab4/mod.py
@@ -0,0 +1,60 @@
+def greeting(name):
+  print("Hello, " + name)
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+
+# import mymodule.py
+# a = mymodule.person1["age"]
+# print(a)
+
+
+
+# import mymodule as mx
+# a = mx.person1["age"]
+# print(a)
+
+
+
+import platform
+x = platform.system()
+print(x)
+
+
+
+import platform
+x = dir(platform)
+print(x)
+
+
+
+def greeting(name):
+  print("Hello, " + name)
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+
+
+# from mymodule import person1
+# print (person1["age"])
+
+
+
+#exercises
+#1
+#import mymodule
+#2
+#import mymodule as mx
+#3
+# import mymodule
+# print(dir(mymodule))
+#4
+#from mymodule import person1
