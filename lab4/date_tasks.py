55256dfaed2cffebbcda892cb41094f3d72037ad
kakostur <tursynbaevakarakat@gmail.com>
2024-02-14 09:54:14 +0600

    date tasks

---
+++ lab4/date_tasks.py
@@ -0,0 +1,37 @@
+#1
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+five_days_ago = current_date - timedelta(days=5)
+print(five_days_ago)
+
+
+#2
+from datetime import datetime, timedelta
+
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+
+print( yesterday)
+print( today)
+print( tomorrow)
+
+
+#3
+from datetime import datetime
+
+current_datetime = datetime.now()
+current_datetime_without_microseconds = current_datetime.replace(microsecond=0)
+print(current_datetime_without_microseconds)
+
+
+
+#4
+from datetime import datetime
+
+date1 = datetime(2023, 5, 20, 10, 30, 0)
+date2 = datetime(2023, 5, 25, 15, 45, 0)
+
+difference_seconds = (date2 - date1).total_seconds()
+print(difference_seconds)
